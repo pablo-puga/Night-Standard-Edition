3eb205e978fa57f82a483044faa2d233c0dcf763
Pablo Puga <pablo.puga.git@gmail.com>
2016-02-15 23:48:03 +0100

    Created the controller for the NotFound route

---
+++ src/NightStandardEdition/Controller/NotFoundController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace NightStandardEdition\Controller;
+
+
+class NotFoundController
+{
+    public function notFoundAction()
+    {
+        echo "This is not the route you are looking for";
+    }
+}
+
