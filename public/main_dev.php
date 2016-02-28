a58600f0ce270ffddbc77969c7b1d2752345929c
Pablo Puga <pablo.puga.git@gmail.com>
2016-02-28 20:56:48 +0100

    Added final versions of the front controllers

--- public/main_dev.php
+++ public/main_dev.php
@@ -3,10 +3,13 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Night\Component\Bootstrap\Bootstrap;
+use Night\Component\Profiling\Profiler;
 use Night\Component\Request\Request;
 
 $bootstrap = new Bootstrap();
 $request   = Request::newFromGlobals();
+
+Profiler::enable();
 
 /**@var $response \Night\Component\Response\Response */
 $response = $bootstrap($request);
