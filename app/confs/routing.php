f16bf89a3bb946487b12bcac40c151e368a880c9
Pablo Puga <pablo.puga.git@gmail.com>
2016-02-28 21:47:02 +0100

    Added final version of the routing files

--- app/confs/routing.php
+++ app/confs/routing.php
@@ -1,16 +1,37 @@
 <?php
 return [
-    'vinos' => [
-        'route' => '/vinos',
+    'home' => [
+        'route' => '/',
         'path' => [
-            'classname' => 'NightStandardEdition\\Controller\\VinosController',
-            'callablemethod' => 'listVinosAction'
+            'classname' => 'NightStandardEdition\Controller\HomeController',
+            'callablemethod' => 'homeAction'
+        ]
+    ],
+    'worldRepository' => [
+        'route' => '/world/cities/{countryCode',
+        'path' => [
+            'classname' => 'NightStandardEdition\Controller\RepositoryController',
+            'callablemethod' => 'listCitiesFromCountryCodeAction'
+        ]
+    ],
+    'worldRepositoryJSON' => [
+        'route' => '/json/world/cities/{countryCode',
+        'path' => [
+            'classname' => 'NightStandardEdition\Controller\RepositoryController',
+            'callablemethod' => 'listCitiesFromCountryCodeActionJSON'
+        ]
+    ],
+    'translations' => [
+        'route' => '/translations/{lang}',
+        'path' => [
+            'classname' => 'NightStandardEdition\Controller\TranslationsController',
+            'callablemethod' => 'translateAction'
         ]
     ],
     'notfound' => [
         'route' => '',
         'path' => [
-            'classname' => 'NightStandardEdition\\Controller\\NotFoundController',
+            'classname' => 'NightStandardEdition\Controller\NotFoundController',
             'callablemethod' => 'notFoundAction'
         ]
     ]
